b4c8cfcaf209c27157cdfcfa5cbca29a08386738
caeslo <ckarakas@condorcet93.fr>
2025-06-04 17:45:11 +0200

    armor and consommable

--- Assets/Scripts/DnD_5e/Item/Consumable.cs
+++ Assets/Scripts/DnD_5e/Item/Consumable.cs
@@ -7,19 +7,29 @@ namespace DnD.DnD_5e
         public string Effect { get; set; }
 
         public Consumable(string name
-                         ,string description = null
-                         ,int weight = 0
-                         ,int value  = 0
-                         ,Harmony HarmonyFlags = 0
+                         , string description = null
+                         , int weight = 0
+                         , int value = 0
+                         , Harmony HarmonyFlags = 0
+
+                         , int nombre = 0
+                         , string effect = null
+                         , List<DamageComponent> damage = null
+
                          )
-                         :base(name
-                              ,description
-                              ,weight
-                              ,value
-                              ,HarmonyFlags
+                         : base(name
+                              , description
+                              , weight
+                              , value
+                              , HarmonyFlags
+                              , nombre
+                              , effect
+                              , damage
                               )
         {
-            
+            nombre = nombre;
+            damage = damage;
+            effect = effect;
         }
 
     }
