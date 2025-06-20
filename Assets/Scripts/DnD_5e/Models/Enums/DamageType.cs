6f55acb05d0f0d1f2b7c0765b7087195e42a199c
Selixy <selenabouloizeau@gmail.com>
2025-06-20 17:01:53 +0200

    réorganisation des fichiers liés à Damges

---
+++ Assets/Scripts/DnD_5e/Models/Enums/DamageType.cs
@@ -0,0 +1,20 @@
+namespace DnD.DnD_5e
+{
+    public enum DamageType
+    {
+        Contondant, 
+        Perforant, 
+        Tranchant, 
+        Force,
+        Feu, 
+        Froid, 
+        Foudre, 
+        Tonnerre,
+        Acide, 
+        Poison, 
+        Radiant, 
+        Nécrotique, 
+        Psychique, 
+        Heal
+    }
+}
